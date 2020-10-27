5ca1987d4e0019dd3c88d902adfcca53689b2a1d
FrancescoDale <fd060862@gmail.com>
2020-10-27 17:19:32 +0100

    creato il blocco che assegna la classe active all'immagine successiva e in caso non sia presente, la assegna alla prima immagine creando così un loop. Fix length e apici che provocavano errore

--- assets/main.js
+++ assets/main.js
@@ -5,10 +5,21 @@ $(document).ready(function() {
     $('.right_arrow').click(function() {
 
         //creo una variabile che prende l'immagine con classe .active
-        var activePic = $(img.active);
+        var activePic = $('img.active');
 
         // all'immagine visualizzata viene tolta la classe .active e la si assegna all'immagine successiva ( se c'è, verificabile con un if/else)
         activePic.removeClass('active');
+
+        //verifica se esiste una img successiva, se esiste gli assegna la classe .active
+
+        if (activePic.next('img').length) {
+            activePic.next('img').addClass('active');
+        }
+
+        // altrimenti, la classe active viene assegnata alla prima immagine in modo da creare un loop
+        else {
+            $('.pics_container :first-child').addClass('active');
+        }
     }
     );
 
