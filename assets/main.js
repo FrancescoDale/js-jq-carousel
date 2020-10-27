aa955ecaacd5dc8ba46824ac107e52210def1e7d
FrancescoDale <fd060862@gmail.com>
2020-10-27 16:57:20 +0100

    cambio classe

--- assets/main.js
+++ assets/main.js
@@ -5,11 +5,12 @@ $(document).ready(function() {
     $('.right_arrow').click(function() {
 
         //creo una variabile che prende l'immagine con classe .active
-        var activePic = 
+        var activePic = $(img.active);
+
+        // all'immagine visualizzata viene tolta la classe .active e la si assegna all'immagine successiva ( se c'è, verificabile con un if/else)
+        activePic.removeClass('active');
     }
-
-
-)
+    );
 
 
 
