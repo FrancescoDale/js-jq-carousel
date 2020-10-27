dcb17f48c8de3a47ff40728e50c0bee05edc2399
FrancescoDale <fd060862@gmail.com>
2020-10-27 17:39:19 +0100

    la freccia sinistra riporta all'immagine precedente

--- assets/main.js
+++ assets/main.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
         var activePic = $('img.active');
 
         // all'immagine visualizzata viene tolta la classe .active e la si assegna all'immagine successiva ( se c'è, verificabile con un if/else)
+
         activePic.removeClass('active');
 
         //verifica se esiste una img successiva, se esiste gli assegna la classe .active
@@ -26,6 +27,23 @@ $(document).ready(function() {
     // blocco che riguarda il click sulla freccia di sinistra
     $('.left_arrow i').click(function() {
 
+        //creo una variabile che prende l'immagine con classe .active
+        var activePic = $('img.active');
+
+        // all'immagine visualizzata viene tolta la classe .active e la si assegna all'immagine precedente ( se c'è, verificabile con un if/else)
+
+        activePic.removeClass('active');
+
+        //verifica se esiste una img precedente, se esiste gli assegna la classe active
+        if (activePic.prev('img').lenght) {
+            activePic.prev('img').addClass('active');
+        }
+
+        // altrimenti, la classe active viene assegnata al'ultima' immagine in modo da creare un loop
+        
+        else {
+            $('.pics_container :last-child').addClass('active');
+        }
 
     }
     );
