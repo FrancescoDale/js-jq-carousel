1e7938094539c79798fd2747ce64ad3af51cbd44
FrancescoDale <fd060862@gmail.com>
2020-10-27 16:52:50 +0100

    prendo il click sulla freccia di destra

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
     console.log('pronto!');
 
+    // blocco che riguarda il click sulla freccia di destra
+    $('.right_arrow').click(function() {
+
+        //creo una variabile che prende l'immagine con classe .active
+        var activePic = 
+    }
+
+
+)
+
+
 
 }
 );
